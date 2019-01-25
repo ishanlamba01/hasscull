c82d0bc2e912889b2b96878ae1e9c2e162dd1b19
Ishan Lamba <LambaI19@pwcs-edu.org>
2019-01-25 13:37:34 -0500

    Updated manipulation file

--- manipulation.hs
+++ manipulation.hs
@@ -1,13 +1,19 @@
 import Data.List
+
 haskellMethodOne :: Int -> String -> String
+
 haskellMethodOne a bs
+
       | even a = bs
+
       | otherwise = reverse bs
 
+
 haskellMethodTwo as b
+
             | even b = as
+
             | otherwise = reverse as
 
-boustrophedon as = [haskellMethodTwo a b | (a, b) <- assignIndices as]
-
-assignIndices as = zip as [0..]
+boustrophedon as = [haskellMethodTwo a b | (a, b) <- zip as [0..]]
+sillyHaskellTupleReverse as = [ (b, a) | (a, b) <- as ]
